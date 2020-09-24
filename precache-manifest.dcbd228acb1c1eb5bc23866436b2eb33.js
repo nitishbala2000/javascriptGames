7e098aed4ce21886ffd8dfb84c081be3f73fb23d
nitish <nitishbala2000@gmail.com>
2020-09-24 15:29:50 +0100

    Updates

---
+++ precache-manifest.dcbd228acb1c1eb5bc23866436b2eb33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73fbdd3dc2aa5e484fdd2074987b3ce6",
+    "url": "/javascriptGames/index.html"
+  },
+  {
+    "revision": "88581e1f990dc1766634",
+    "url": "/javascriptGames/static/css/main.30ca6eb7.chunk.css"
+  },
+  {
+    "revision": "38856c94051c3c59753e",
+    "url": "/javascriptGames/static/js/2.1a0244fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/javascriptGames/static/js/2.1a0244fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88581e1f990dc1766634",
+    "url": "/javascriptGames/static/js/main.5b8d0f55.chunk.js"
+  },
+  {
+    "revision": "3b414dd78dbfe0910130",
+    "url": "/javascriptGames/static/js/runtime-main.33565cf0.js"
+  },
+  {
+    "revision": "ec4816fb624758db55d6ec0944d3739b",
+    "url": "/javascriptGames/static/media/background.ec4816fb.jpg"
+  },
+  {
+    "revision": "c02c3cb83f621f0914694e00b458945f",
+    "url": "/javascriptGames/static/media/catcher1.c02c3cb8.png"
+  },
+  {
+    "revision": "05c73bfad68b8c1447d96d0e65791677",
+    "url": "/javascriptGames/static/media/catcher2.05c73bfa.png"
+  },
+  {
+    "revision": "1c2a5d18706ed867a162acb5fea23c3d",
+    "url": "/javascriptGames/static/media/food.1c2a5d18.png"
+  },
+  {
+    "revision": "1d29b853373632b0f0424978c1b416bc",
+    "url": "/javascriptGames/static/media/tile.1d29b853.png"
+  }
+]);
